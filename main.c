d7919180f8a48a6e145ab4fa1a394781dea7b7f4
pumple <ljhfree99@naver.com>
2023-01-23 23:05:58 +0900

    add input processing

--- main.c
+++ main.c
@@ -5,16 +5,23 @@
 static int	get_frac_argu(void)
 {
 	char	*in;
+	char	**in_arr;
 
 	ft_printf("Input [fractal_type] [fractal_argument]\n");
 	in = get_next_line(0);
-	
-	return (*e);
+	if (in == (char *)1)
+		return (print_try_again());
+	else
+	{
+		in_arr = ft_split(in, ' ');
+		free(in);
+	}
+	return (0);
 }
 
 int	main()
 {
-	int		e;
+	int	e;
 
 	e = 0;
 	get_frac_argu();
